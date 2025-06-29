fe1bcb4a1142dec9ed52b0bd1f2cf33189ec5526
Matheus Moraes <155101411+maatbm@users.noreply.github.com>
2025-06-28 22:43:24 -0300

    projeto criado

---
+++ src/test/java/com/teach_3035/teachgram_back/TeachgramBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teach_3035.teachgram_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeachgramBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
