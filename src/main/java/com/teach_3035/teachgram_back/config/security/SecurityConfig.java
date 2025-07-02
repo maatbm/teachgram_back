5edb3346ba8ca9ed9da6ffbad90f052f074c840c
Matheus Moraes <155101411+maatbm@users.noreply.github.com>
2025-07-01 22:20:30 -0300

    add: filtro do token jwt adicionado ao security config

--- src/main/java/com/teach_3035/teachgram_back/config/security/SecurityConfig.java
+++ src/main/java/com/teach_3035/teachgram_back/config/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.teach_3035.teachgram_back.config.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -13,10 +14,13 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    @Autowired
+    private JWTAuthenticationFilter JWTAuthenticationFilter;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity security) throws Exception {
@@ -29,7 +33,9 @@ public class SecurityConfig {
                                 .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
                                 .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                                 .anyRequest().authenticated()
-                );
+                )
+                .addFilterBefore(JWTAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
         return security.build();
     }
 
