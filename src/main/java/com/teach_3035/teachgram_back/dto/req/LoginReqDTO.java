d68a99a9fb7c5fb07f0ab6da0e9af57e72466a30
Matheus Moraes <155101411+maatbm@users.noreply.github.com>
2025-07-01 21:37:58 -0300

    add: dto de request para login

---
+++ src/main/java/com/teach_3035/teachgram_back/dto/req/LoginReqDTO.java
@@ -0,0 +1,18 @@
+package com.teach_3035.teachgram_back.dto.req;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record LoginReqDTO(
+
+        @NotBlank(message = "Email cannot be blank")
+        @Size(min = 5, max = 150, message = "Email must be between 5 and 150 characters")
+        @Email(message = "Invalid email format")
+        String email,
+
+        @NotBlank(message = "Password cannot be blank")
+        @Size(min = 6, max = 20, message = "Password must be between 6 and 20 characters")
+        String password
+) {
+}
