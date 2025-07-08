3c32825e7d27e096b429a5172671524000c96a89
Matheus Moraes <155101411+maatbm@users.noreply.github.com>
2025-07-08 19:11:37 -0300

    Create UserRepository.java

---
+++ src/main/java/com/teach3035/teachgram_back/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.teach3035.teachgram_back.repository;
+
+import com.teach3035.teachgram_back.model.UserModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserModel, Long> {
+    Optional<UserModel> findByMailOrUsernameOrPhone(String mail, String username, String phone);
+}
