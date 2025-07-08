6d07f70873f15d2d146f1b1076e8eafe6179d193
Matheus Moraes <155101411+maatbm@users.noreply.github.com>
2025-07-08 14:52:42 -0300

    add: postModel

---
+++ src/main/java/com/teach3035/teachgram_back/model/PostModel.java
@@ -0,0 +1,54 @@
+package com.teach3035.teachgram_back.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.SoftDelete;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "posts")
+@SoftDelete
+@Getter
+@Setter
+@NoArgsConstructor
+@RequiredArgsConstructor
+public class PostModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.NONE)
+    private Long id;
+
+    @Column(nullable = false)
+    @NonNull
+    private String title;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    @NonNull
+    private String description;
+
+    @Column(name = "photo_link")
+    private String photoLink;
+
+    @Column(name = "video_link")
+    private String videoLink;
+
+    @Column(name = "private", nullable = false)
+    private Boolean isPrivate;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    private UserModel user;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    @CreationTimestamp
+    @Setter(AccessLevel.NONE)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    @UpdateTimestamp
+    @Setter(AccessLevel.NONE)
+    private LocalDateTime updatedAt;
+}
