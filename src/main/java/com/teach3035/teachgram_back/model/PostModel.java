16a5f35afde4f4ca0a5e117ac873aaf3f34a7231
Matheus Moraes <155101411+maatbm@users.noreply.github.com>
2025-07-08 16:06:41 -0300

    add: likes

--- src/main/java/com/teach3035/teachgram_back/model/PostModel.java
+++ src/main/java/com/teach3035/teachgram_back/model/PostModel.java
@@ -44,6 +44,9 @@ public class PostModel {
     @NonNull
     private UserModel user;
 
+    @Column(nullable = false)
+    private Long likes;
+
     @Column(name = "created_at", nullable = false, updatable = false)
     @CreationTimestamp
     @Setter(AccessLevel.NONE)
