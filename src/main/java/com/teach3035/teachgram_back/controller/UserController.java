f1d34ff1933e2c9115bc502132539214fb8f441d
Matheus Moraes <155101411+maatbm@users.noreply.github.com>
2025-07-08 18:42:52 -0300

    Create UserController.java

---
+++ src/main/java/com/teach3035/teachgram_back/controller/UserController.java
@@ -0,0 +1,23 @@
+package com.teach3035.teachgram_back.controller;
+
+import com.teach3035.teachgram_back.dto.req.SignupUserReqDTO;
+import com.teach3035.teachgram_back.dto.res.UserResDTO;
+import com.teach3035.teachgram_back.service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    @Autowired
+    UserService userService;
+
+    @PostMapping("/signup")
+    public UserResDTO signup (@Valid @RequestBody SignupUserReqDTO request){
+        return userService.signup(request);
+    }
+}
