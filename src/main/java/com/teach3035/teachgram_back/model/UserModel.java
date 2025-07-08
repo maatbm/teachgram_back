d289878a561d32b9422798cdec0b4ee52e815608
Matheus Moraes <155101411+maatbm@users.noreply.github.com>
2025-07-08 14:52:51 -0300

    add: userModel

--- src/main/java/com/teach3035/teachgram_back/model/UserModel.java
+++ src/main/java/com/teach3035/teachgram_back/model/UserModel.java
@@ -2,7 +2,16 @@ package com.teach3035.teachgram_back.model;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.SoftDelete;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collection;
 
 @Entity
 @Table(name = "users")
@@ -11,11 +20,77 @@ import org.hibernate.annotations.SoftDelete;
 @Setter
 @NoArgsConstructor
 @RequiredArgsConstructor
-public class UserModel {
+public class UserModel implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Setter(AccessLevel.NONE)
     private Long id;
 
+    @Column(nullable = false)
+    @NonNull
+    private String name;
 
+    @Column(nullable = false, unique = true)
+    @NonNull
+    private String mail;
+
+    @Column(name = "username", nullable = false, unique = true)
+    @NonNull
+    private String userName;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    @NonNull
+    private String description;
+
+    @Column(nullable = false, unique = true)
+    @NonNull
+    private String phone;
+
+    @Column(nullable = false)
+    @NonNull
+    private String password;
+
+    @Column(name = "profle_link", nullable = false)
+    @NonNull
+    private String profileLink;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    @CreationTimestamp
+    @Setter(AccessLevel.NONE)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    @UpdateTimestamp
+    @Setter(AccessLevel.NONE)
+    private LocalDateTime updatedAt;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return new ArrayList<SimpleGrantedAuthority>();
+    }
+
+    @Override
+    public String getUsername() {
+        return this.mail;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
