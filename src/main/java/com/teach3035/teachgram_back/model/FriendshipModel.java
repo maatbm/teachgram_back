c36d7460fbb57a98f48837f58ec02b0ce80d7fe9
Matheus Moraes <155101411+maatbm@users.noreply.github.com>
2025-07-08 16:56:53 -0300

    Create FriendshipModel.java

---
+++ src/main/java/com/teach3035/teachgram_back/model/FriendshipModel.java
@@ -0,0 +1,39 @@
+package com.teach3035.teachgram_back.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.SoftDelete;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "frindships")
+@SoftDelete
+@Getter
+@NoArgsConstructor
+@RequiredArgsConstructor
+public class FriendshipModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id_1", nullable = false, updatable = false)
+    @NonNull
+    private UserModel user1;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id_2", nullable = false, updatable = false)
+    @NonNull
+    private UserModel user2;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
