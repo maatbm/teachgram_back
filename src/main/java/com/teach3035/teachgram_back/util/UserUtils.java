a78ec419d1be759a53d72c849dc37d79abf71399
Matheus Moraes <155101411+maatbm@users.noreply.github.com>
2025-07-09 10:57:04 -0300

    add: classe utilitária para consulta por email

---
+++ src/main/java/com/teach3035/teachgram_back/util/UserUtils.java
@@ -0,0 +1,18 @@
+package com.teach3035.teachgram_back.util;
+
+import com.teach3035.teachgram_back.model.UserModel;
+import com.teach3035.teachgram_back.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserUtils {
+    @Autowired
+    UserRepository userRepository;
+
+    public UserModel getUserByEmail(String email) {
+        return userRepository
+                .findByMail(email)
+                .orElseThrow(() -> new RuntimeException("User not found with email " + email));
+    }
+}
