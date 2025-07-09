04a51f25fde4d71aee120c12be09665cc5ce3960
Matheus Moraes <155101411+maatbm@users.noreply.github.com>
2025-07-09 18:56:03 -0300

    fix: likes nulos

--- src/main/java/com/teach3035/teachgram_back/model/PostModel.java
+++ src/main/java/com/teach3035/teachgram_back/model/PostModel.java
@@ -46,7 +46,6 @@ public class PostModel {
     @NonNull
     private UserModel user;
 
-    @Column(nullable = false)
     private Long likes;
 
     @Column(name = "created_at", nullable = false, updatable = false)
