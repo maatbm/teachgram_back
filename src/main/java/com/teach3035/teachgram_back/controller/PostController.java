e8cc2566361c33f03ce385c63ee12c3e7ae17751
Matheus Moraes <155101411+maatbm@users.noreply.github.com>
2025-07-09 17:55:51 -0300

    Create PostController.java

---
+++ src/main/java/com/teach3035/teachgram_back/controller/PostController.java
@@ -0,0 +1,23 @@
+package com.teach3035.teachgram_back.controller;
+
+import com.teach3035.teachgram_back.dto.req.PostReqDTO;
+import com.teach3035.teachgram_back.dto.res.PostResDTO;
+import jakarta.validation.Valid;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/post")
+public class PostController {
+    @PostMapping
+    public PostResDTO createPost(
+            @AuthenticationPrincipal UserDetails user,
+            @Valid @RequestBody PostReqDTO request
+    ) {
+        return postService.createPost(user.getUsername(), request);
+    }
+}
