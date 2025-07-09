aa725c04f2ee99859259c3923365967292af15f4
Matheus Moraes <155101411+maatbm@users.noreply.github.com>
2025-07-08 21:13:31 -0300

    add: adicionando bean do authentication manager

--- src/main/java/com/teach3035/teachgram_back/config/security/SecurityConfig.java
+++ src/main/java/com/teach3035/teachgram_back/config/security/SecurityConfig.java
@@ -3,6 +3,8 @@ package com.teach3035.teachgram_back.config.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -33,5 +35,10 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
     }
 }
