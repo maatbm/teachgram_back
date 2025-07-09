8b7813ed5b75935315e2918d27bd1e3b9773aa00
Matheus Moraes <155101411+maatbm@users.noreply.github.com>
2025-07-09 18:46:42 -0300

    Create PostService.java

---
+++ src/main/java/com/teach3035/teachgram_back/service/PostService.java
@@ -0,0 +1,44 @@
+package com.teach3035.teachgram_back.service;
+
+import com.teach3035.teachgram_back.dto.req.PostReqDTO;
+import com.teach3035.teachgram_back.dto.res.PostResDTO;
+import com.teach3035.teachgram_back.model.PostModel;
+import com.teach3035.teachgram_back.model.UserModel;
+import com.teach3035.teachgram_back.repository.PostRepository;
+import com.teach3035.teachgram_back.util.UserUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PostService {
+    @Autowired
+    UserUtils userUtils;
+    @Autowired
+    PostRepository postRepository;
+
+    public PostResDTO createPost(String email, PostReqDTO request) {
+        UserModel user = userUtils.getUserByEmail(email);
+        PostModel post = new PostModel(
+                request.title(),
+                request.description(),
+                request.photoLink(),
+                request.videoLink(),
+                request.isPrivate(),
+                user
+        );
+        postRepository.save(post);
+        return this.postResDTOBuilder(post);
+    }
+
+    private PostResDTO postResDTOBuilder(PostModel post){
+        return new PostResDTO(
+                post.getId(),
+                post.getTitle(),
+                post.getDescription(),
+                post.getPhotoLink(),
+                post.getVideoLink(),
+                post.getLikes(),
+                post.getCreatedAt()
+        );
+    }
+}
