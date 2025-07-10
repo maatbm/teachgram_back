9847598fb8b350033e4d8eda260afc41f33a8422
Matheus Moraes <155101411+maatbm@users.noreply.github.com>
2025-07-10 13:53:03 -0300

    fix: excessão correta lançada

--- src/main/java/com/teach3035/teachgram_back/util/UserUtils.java
+++ src/main/java/com/teach3035/teachgram_back/util/UserUtils.java
@@ -3,6 +3,7 @@ package com.teach3035.teachgram_back.util;
 import com.teach3035.teachgram_back.model.UserModel;
 import com.teach3035.teachgram_back.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,6 +14,6 @@ public class UserUtils {
     public UserModel getUserByEmail(String email) {
         return userRepository
                 .findByMail(email)
-                .orElseThrow(() -> new RuntimeException("User not found"));
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
     }
 }
