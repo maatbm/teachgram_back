1d33bb9ac7af9403d4eff2947b4c05ec9baf7799
Matheus Moraes <155101411+maatbm@users.noreply.github.com>
2025-07-11 19:37:31 -0300

    Update UserDetailsServiceImpl.java

--- src/main/java/com/teach3035/teachgram_back/config/security/UserDetailsServiceImpl.java
+++ src/main/java/com/teach3035/teachgram_back/config/security/UserDetailsServiceImpl.java
@@ -1,6 +1,6 @@
 package com.teach3035.teachgram_back.config.security;
 
-import com.teach3035.teachgram_back.util.UserUtils;
+import com.teach3035.teachgram_back.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -10,10 +10,12 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
     @Autowired
-    UserUtils userUtils;
+    UserRepository userRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return userUtils.getUserByEmail(username);
+        return userRepository
+                .findByMail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Email não encontrado"));
     }
 }
