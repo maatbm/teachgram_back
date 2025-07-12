8342452f0048026bd8555d414d85b49da54fecac
Matheus Moraes <155101411+maatbm@users.noreply.github.com>
2025-07-12 14:10:50 -0300

    mudança para jpql

--- src/main/java/com/teach3035/teachgram_back/repository/PostRepository.java
+++ src/main/java/com/teach3035/teachgram_back/repository/PostRepository.java
@@ -5,12 +5,12 @@ import com.teach3035.teachgram_back.model.UserModel;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.NativeQuery;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PostRepository extends JpaRepository<PostModel, Long> {
-    @NativeQuery("SELECT * FROM posts WHERE private = false AND deleted = false")
+    @Query("SELECT p FROM PostModel p WHERE p.isPrivate = false")
     Page<PostModel> getFeedPosts(Pageable pageable);
 
     Page<PostModel> findByUser(UserModel user, Pageable pageable);
