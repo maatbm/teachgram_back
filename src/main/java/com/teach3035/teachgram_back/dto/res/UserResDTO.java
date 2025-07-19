2ddae43aa746ade2ded69ef53b510ccf0ee0c41a
Matheus Moraes <155101411+maatbm@users.noreply.github.com>
2025-07-19 19:13:42 -0300

    fix: nome da variavel corrigida

--- src/main/java/com/teach3035/teachgram_back/dto/res/UserResDTO.java
+++ src/main/java/com/teach3035/teachgram_back/dto/res/UserResDTO.java
@@ -3,7 +3,7 @@ package com.teach3035.teachgram_back.dto.res;
 public record UserResDTO(
         Long id,
         String name,
-        String email,
+        String mail,
         String username,
         String description,
         String phone,
